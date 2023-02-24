47659211774542da5da319f84561b1f3321955db
“phat” <phangiaan20042k4@gmal.com>
2023-02-24 10:32:51 +0700

    l

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, sum;
+
+    printf("Nhập số 1: ");
+    scanf("%d", &a);
+    printf("Nhập số 2: ");
+    scanf("%d", &b);
+
+    sum = a + b;
+
+    printf("Tổng của a và b là: %d \n",sum);
+
+    return 0;
+}
